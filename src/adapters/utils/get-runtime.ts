604011e118719a4f419ff0aa5fc03c1ab12313c2
fgc17 <fgcoelho1709@outlook.com>
2025-01-05 05:02:17 -0300

    feat: getRuntime util

---
+++ src/adapters/utils/get-runtime.ts
@@ -0,0 +1,26 @@
+declare const Bun: any;
+declare const Deno: any;
+
+export const getRuntime = () => {
+  let runtime: string = "";
+
+  if (process.release.name === "node") {
+    runtime = "node";
+  }
+
+  if (typeof Bun !== "undefined") {
+    runtime = "bun";
+  }
+
+  if (typeof Deno !== "undefined") {
+    runtime = "deno";
+  }
+
+  const supportedRuntimes = ["node", "bun"];
+
+  if (!supportedRuntimes.includes(runtime)) {
+    throw new Error(`Unsupported runtime: ${runtime}`);
+  }
+
+  return runtime;
+};
