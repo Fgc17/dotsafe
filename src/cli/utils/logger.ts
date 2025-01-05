77fcbf8af4a1dc140aab38ea227938e202dc5c43
fgc17 <fgcoelho1709@outlook.com>
2025-01-05 05:04:23 -0300

    fix: slash n for better loggin readability

--- src/cli/utils/logger.ts
+++ src/cli/utils/logger.ts
@@ -1,16 +1,18 @@
 import chalk from "chalk";
 
+const line = (message: string) => "\n" + "🔒 [ts-env] " + message + "\n";
+
 const error = (error: any, message: string) => {
   console.error(error);
-  console.log(chalk.bgRed("🔒 [ts-env] Error: " + message));
+  console.log(chalk.bgRed(line(message)));
 };
 
 const info = (message: string) => {
-  console.log(chalk.bgBlue("🔒 [ts-env] " + message));
+  console.log(chalk.bgBlue(line(message)));
 };
 
 const success = (message: string) => {
-  console.log(chalk.bgHex("#052e16").hex("#22c55e")("🔒 [ts-env] " + message));
+  console.log(chalk.bgHex("#052e16").hex("#22c55e")(line(message)));
 };
 
 export const logger = {
