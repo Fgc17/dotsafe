9976ddb052ef14b79351db4ac2942a791d355c77
fgc17 <fgcoelho1709@outlook.com>
2025-02-07 14:31:28 -0300

    fix: can't access tolowercase on undefined

--- packages/fatima/src/core/utils/fatima-env.ts
+++ packages/fatima/src/core/utils/fatima-env.ts
@@ -2,7 +2,7 @@ export const fatimaEnv = {
   get() {
     return process.env.FATIMA_ENV!;
   },
-  set(env: string) {
+  set(env: string = "missing_environment") {
     process.env.FATIMA_ENV = env.toLocaleLowerCase();
   },
 };
