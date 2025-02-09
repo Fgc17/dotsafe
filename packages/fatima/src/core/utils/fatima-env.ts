16521ca411c99a71e4c9d3c98258d945c34b1c01
fgc17 <fgcoelho1709@outlook.com>
2025-02-09 14:33:14 -0300

    fix: forbidden not null assertion

--- packages/fatima/src/core/utils/fatima-env.ts
+++ packages/fatima/src/core/utils/fatima-env.ts
@@ -1,8 +1,8 @@
 export const fatimaEnv = {
-  get() {
-    return process.env.FATIMA_ENV!;
-  },
-  set(env: string = "missing_environment") {
-    process.env.FATIMA_ENV = env.toLocaleLowerCase();
-  },
+	get() {
+		return process.env.FATIMA_ENV as string;
+	},
+	set(env: string = "missing_environment") {
+		process.env.FATIMA_ENV = env.toLocaleLowerCase();
+	},
 };
