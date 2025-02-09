f794b51be2ae6ac41dca75e7214d53c8bd9c882c
fgc17 <fgcoelho1709@outlook.com>
2025-02-09 14:33:30 -0300

    fix: trivially inferred  type

--- packages/fatima/src/core/utils/fatima-env.ts
+++ packages/fatima/src/core/utils/fatima-env.ts
@@ -2,7 +2,7 @@ export const fatimaEnv = {
 	get() {
 		return process.env.FATIMA_ENV as string;
 	},
-	set(env: string = "missing_environment") {
+	set(env = "missing_environment") {
 		process.env.FATIMA_ENV = env.toLocaleLowerCase();
 	},
 };
