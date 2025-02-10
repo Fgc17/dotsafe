36c374a4bf03671a9030d2ce809678baf4b1cdb2
fgc17 <fgcoelho1709@outlook.com>
2025-02-10 08:59:42 -0300

    fix (fatima): revert fatimaEnv default value

--- packages/fatima/src/core/utils/fatima-env.ts
+++ packages/fatima/src/core/utils/fatima-env.ts
@@ -2,7 +2,7 @@ export const fatimaEnv = {
 	get() {
 		return process.env.FATIMA_ENV as string;
 	},
-	set(env = "missing_environment") {
-		process.env.FATIMA_ENV = env.toLocaleLowerCase();
+	set(env?: string) {
+		process.env.FATIMA_ENV = env?.toLocaleLowerCase();
 	},
 };
