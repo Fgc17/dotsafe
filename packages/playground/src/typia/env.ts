51d96aec0e733b718ced5ada78e5f52029a04121
fgc17 <fgcoelho1709@outlook.com>
2025-02-12 17:15:45 -0300

    playground

--- packages/playground/src/typia/env.ts
+++ packages/playground/src/typia/env.ts
@@ -1,34 +1,34 @@
 import {
-	createEnv,
-	type ServerEnvRecord,
-	type EnvType as FatimaEnvType,
-	type EnvRecord as FatimaEnvRecord,
-	type PrimitiveEnvType as FatimaPrimitiveEnvType,
-} from "fatima/env";
+  createEnv,
+  type ServerEnvRecord,
+  type EnvType as FatimaEnvType,
+  type EnvRecord as FatimaEnvRecord,
+  type PrimitiveEnvType as FatimaPrimitiveEnvType,
+} from 'fatima/env';
 
-export type EnvKeys = "NEXT_PUBLIC_API_URL" | "NODE_ENV" | "TEST" | "TZ";
-
-export type EnvRecord<V = string> = FatimaEnvRecord<EnvKeys, V>;
-
-export interface EnvClass {
-	NEXT_PUBLIC_API_URL: string;
-	NODE_ENV: string;
-	TEST: string;
-	TZ: string;
+export interface EnvObject {
+  "NODE_ENV": string;
+  "TZ": string;
+  "NEXT_PUBLIC_API_URL": string;
+  "TEST": string;
 }
 
-type PrimitiveEnvType = FatimaPrimitiveEnvType<EnvKeys>;
-export type EnvType<T extends PrimitiveEnvType> = FatimaEnvType<EnvKeys, T>;
+export type EnvKeys = keyof EnvObject;
 
-type Env = ServerEnvRecord<EnvKeys, "NEXT_PUBLIC_">;
+export type EnvRecord<V = string> = FatimaEnvRecord<EnvObject, V>;
+
+type PrimitiveEnvType = FatimaPrimitiveEnvType<EnvObject>;
+export type EnvType<T extends PrimitiveEnvType> = FatimaEnvType<EnvObject, T>;
+
+type Env = ServerEnvRecord<EnvKeys, "NEXT_PUBLIC_">
 
 export const env = createEnv({ isServer: undefined }) as Env;
 
 import { createPublicEnv, type PublicEnvRecord } from "fatima/env";
-type PublicEnv = PublicEnvRecord<EnvKeys, "NEXT_PUBLIC_">;
-export const publicEnv = createPublicEnv({
-	publicPrefix: "NEXT_PUBLIC_",
-	publicVariables: {
-		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL as string,
-	},
+type PublicEnv = PublicEnvRecord<EnvKeys, "NEXT_PUBLIC_">
+export const publicEnv = createPublicEnv(  {
+  publicPrefix: "NEXT_PUBLIC_",
+  publicVariables: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL as string
+  }
 }) as PublicEnv;
